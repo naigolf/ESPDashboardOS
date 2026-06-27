abc1ede31e1ab3b3bb67dbdae58f581810243459
nai golf <niy_golf@hotmail.com>
2026-06-27 22:40:53 +0700

    Update ESPDashboardOS.ino

--- ESPDashboardOS.ino
+++ ESPDashboardOS.ino
@@ -1,1 +1,21 @@
 
+#include "Config.h"
+#include "Display.h"
+#include "WifiManager.h"
+#include "Dashboard.h"
+
+void setup()
+{
+  Serial.begin(115200);
+
+  display.begin();
+
+  wifi.begin();
+
+  dashboard.begin();
+}
+
+void loop()
+{
+  dashboard.loop();
+}
